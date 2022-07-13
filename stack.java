8be7d2a94d84cf2e1d9c20a923e438c76b3e1fe6
pradeep <pradeepghosh17@hotmail.com>
2022-07-13 23:07:13 +0530

    minor fix on stack

--- stack.java
+++ stack.java
@@ -1,7 +1,7 @@
 /* Stack using array */
  class Stack {
     private int top = -1;
-    int array[];
+    private int array[];
     private int capacity;
     Stack(int capacity){
         this.capacity = capacity;
@@ -9,7 +9,7 @@
     }
     /* push value on the top position */
     public void push(int data){
-        if(capacity-1 <= top)
+        if(isFull())
             System.out.println("stack overflow:");
         else{
             top++;
@@ -18,33 +18,51 @@
     }
     /* retrun the top value with removing */
     public int pop(){
-        if(top == -1) {
+        if(isEmpty()) {
             System.out.println("stack underflow:");
             return -1;
         }
         else{
             int value = array[top];
             top--;
-            System.out.println(value);
              return value;
         }
     }
+    /* retrun true if stack is empty else return false */
+    public boolean isEmpty(){
+        return top == -1;
+    }
+    /* retrun true if stack is full else return false */
+    public boolean isFull(){
+        return capacity-1 <= top;
+    }
     /* retrun the top value without removing */
     public int peek(){
+        if(isEmpty()) {
+            System.out.println("stack underflow:");
+            return -1;
+        }
         return array[top];
+    }
+    /* print the stack */
+    public void printStack(){
+        for(int i=0;i<=top;i++){
+            System.out.println(array[i]);
+        }
     }
 }
 
 class Stackmain{
     public static void main(String[] args) {
         Stack stack = new Stack(5);
+        //stack.pop();          // stack underflow
         stack.push(10);
         stack.push(20);
         stack.push(30);
         stack.push(40);
         stack.push(50);
-        stack.pop();
-        stack.pop();
-        stack.peek();
+      //  stack.push(60); // stack overflow
+        
+        stack.printStack();
     }
 }
